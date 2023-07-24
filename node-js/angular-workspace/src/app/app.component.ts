509c30a770e4d3ad8f1dff81a66b704de4da13a7
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-07-24 17:40:03 -0600

    📅  | Mon Jul 24 17:40:03 MDT 2023

--- node-js/angular-workspace/src/app/app.component.ts
+++ node-js/angular-workspace/src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { ipcRenderer } from 'electron';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-workspace';
+
+  constructor() {
+    ipcRenderer;
+  }
 }
